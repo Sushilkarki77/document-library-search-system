0d2ce7cafd857624a207cb4ced7c23fc4b50708a
Sushil <sushilkarki352@gmail.com>
2025-08-19 17:40:29 -0500

    Configure Authentication

--- apps/Document-Library-Search-System/src/app/app.config.ts
+++ apps/Document-Library-Search-System/src/app/app.config.ts
@@ -7,10 +7,12 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { API_URL } from "@document-library-search-system/Common";
 import { environment } from '../environments/environment';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
      { provide: API_URL, useValue: environment.api_url },
+     provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
