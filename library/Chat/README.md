1ea7937c44cd1e4f74754ac1878007b8713e485a
Sushil <sushilkarki352@gmail.com>
2025-09-02 12:49:17 -0500

    Create chat library

---
+++ library/Chat/README.md
@@ -0,0 +1,7 @@
+# Chat
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test Chat` to execute the unit tests.
---
+++ library/Chat/src/lib/Chat/Chat.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-chat',
+  imports: [],
+  templateUrl: './Chat.html',
+  styleUrl: './Chat.css',
+})
+export class Chat {}
